fb46ff857c3c2b84786dd7b4b5e433487fc874a2
Umang Dalvadi <umangdalvadi8@gmail.com>
2025-03-04 19:15:54 +0530

    add

--- models/user.js
+++ models/user.js
@@ -19,6 +19,8 @@ const userSchema = new Schema({
     phone: {
         type: String,
         required: true,
+        unique: true,
+        trim: true
     },
     password: {
         type: String,
